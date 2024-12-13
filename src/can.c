9250e1f7b4bf658bf14b5f27e6b3f940f5628fad
Jonathan Warner <warnerjon12@gmail.com>
2024-12-13 10:24:56 -0700

    Combined receiver and transmitter into can.c

--- src/can.c
+++ src/can.c
@@ -3,20 +3,27 @@
 #include <stdio.h>
 #include <pico/stdlib.h>
 
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
 static struct can2040 cbus;
 
-void can2040_cb(struct can2040 *cd, uint32_t notify, struct can2040_msg *msg);
+QueueHandle_t message_queue = NULL;
+
+static void can2040_cb(struct can2040 *cd, uint32_t notify, struct can2040_msg *msg)
+{
+    if (message_queue) {
+        xQueueSendToBackFromISR(message_queue, msg, NULL);
+    }
+}
 
 static void PIOx_IRQHandler(void)
 {
     can2040_pio_irq_handler(&cbus);
 }
 
-int canbus_transmit(struct can2040_msg *msg) {
-    return can2040_transmit(&cbus, msg);
-}
-
-void canbus_setup(void)
+static void canbus_setup(void)
 {
     uint32_t pio_num = 0;
     uint32_t sys_clock = 125000000, bitrate = 500000;
@@ -33,4 +40,51 @@ void canbus_setup(void)
 
     // Start canbus
     can2040_start(&cbus, sys_clock, bitrate, gpio_rx, gpio_tx);
+}
+
+void transmit_task(__unused void *params) {
+    int mid = 0;
+    struct can2040_msg msg;
+    char msg_buf[256];
+
+    for (;;) {
+        sprintf(msg_buf, "This is message #%d.", mid++);
+        size_t n = strlen(msg_buf) + 1;
+        msg.dlc = 8;
+        // this may send a few extra bytes, but it's fine since the string is null-terminated
+        for (size_t i = 0; i <= n / 8; i++) {
+            memcpy(msg.data, msg_buf + 8*i, msg.dlc);
+            while (can2040_transmit(&cbus, &msg) < 0) sleep_ms(10);
+        }
+        vTaskDelay(1000);
+    }
+}
+
+void receive_task(__unused void *params) {
+    struct can2040_msg msg;
+
+    message_queue = xQueueCreate(100, sizeof(struct can2040_msg));
+
+    for (;;) {
+        if (xQueueReceive(message_queue, &msg, portMAX_DELAY) != pdTRUE) continue;
+        char buf[9] = {0};
+        memcpy(msg.data, buf, msg.dlc);
+        printf(buf);
+        if (strlen(buf) < msg.dlc) printf("\n");
+    }
+}
+
+int main( void )
+{
+    stdio_init_all();
+    canbus_setup();
+    const char *rtos_name;
+    rtos_name = "FreeRTOS";
+    TaskHandle_t rtask, ttask;
+    xTaskCreate(transmit_task, "TransmitThread",
+                configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1UL, &ttask);
+    xTaskCreate(receive_task, "ReceiveThread",
+                configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2UL, &rtask);
+    vTaskStartScheduler();
+    return 0;
 }
